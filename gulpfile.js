1170b5862478d120a151ab606042869482b20508
Tan Li Hau <lhtan93@gmail.com>
2016-04-18 16:44:45 +0800

    update gulp flow

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,54 @@
 'use strict';
 
-var gulp = require('gulp'),
-    tslint = require('gulp-tslint'),
-    tsc = require('gulp-typescript');
+var        gulp = require('gulp'),
+          merge = require('merge-stream'),
+            gtb = require('gulp-typescript-babel'),
+         concat = require('gulp-concat'),
+ autopolyfiller = require('gulp-autopolyfiller'),
+          order = require('gulp-order');
 
 var config = {
-    typescript: './lib/ts/*.ts',
+    typescriptClient: './lib/ts/client.ts',
+    typescriptServer: './lib/ts/server.ts',
     typescriptDef: './typings/*.ts',
     output: './lib/out',
 };
 
 /**
- * Lint all custom TypeScript files.
- */
-gulp.task('ts-lint', function () {
-    return gulp.src(config.typescript)
-        .pipe(tslint())
-        .pipe(tslint.report('prose'));
-});
-
-/**
  * Compile TypeScript and include references to library and app .d.ts files.
  */
-gulp.task('compile-ts', function () {
-    return gulp.src(config.typescript)
-                       .pipe(tsc({
-                           declaration: true,
-                           noExternalResolve: true,
-                       }))
-                       .pipe(gulp.dest(config.output));
+var babelOpt = {presets: ['es2015']};
+var tsOpt = {incremental: true, configFile: 'tsconfig.json', reporter: gtb.tsReporter.defaultReporter()};
+gulp.task('compile-ts:client', function () {
+    var client = gulp.src(config.typescriptClient)
+              .pipe(gtb(tsOpt, babelOpt))
+              .pipe(concat('client.js'));
+    var polyfill = client.pipe(autopolyfiller('client-polyfill.js'));
+    return merge(polyfill, client)
+                .pipe(order([
+                        'client-polyfill.js',
+                        'client.js'
+                    ]))
+               .pipe(concat('client.js'))
+               .pipe(gulp.dest(config.output));
 });
+gulp.task('compile-ts:server', function () {
+    var server = gulp.src(config.typescriptServer)
+              .pipe(gtb(tsOpt, babelOpt))
+              .pipe(concat('server.js'));
+    var polyfill = server.pipe(autopolyfiller('server-polyfill.js'));
+    return merge(polyfill, server)
+                .pipe(order([
+                        'server-polyfill.js',
+                        'server.js'
+                    ]))
+               .pipe(concat('server.js'))
+               .pipe(gulp.dest(config.output));
+});
+gulp.task('compile-ts', ['compile-ts:client', 'compile-ts:server'])
 
 gulp.task('watch', function() {
-    gulp.watch([config.typescript], ['ts-lint', 'compile-ts']);
+    gulp.watch([config.typescriptClient, config.typescriptServer], ['compile-ts']);
 });
 
-gulp.task('default', ['ts-lint', 'compile-ts']);
+gulp.task('default', ['compile-ts']);
